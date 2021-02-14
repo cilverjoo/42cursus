78878c61482e77a630c7934f743f7a62430b2157
eunju-silver <yorkessw@gmail.com>
2021-02-15 00:22:40 +0900

    nginx wordpress phpmyadmin connection success

--- ft_services/setup.sh
+++ ft_services/setup.sh
@@ -1,27 +1,47 @@
 #!/bin/bash
 
-# Colors.
-GREEN='\e[0;32m'
-BLUE='\e[0;34m'
-END='\e[0;0m'
-
 minikube delete
 
 #start minikube with docker.
 minikube start --driver=docker
 
+minikube addons enable metrics-server
+minikube addons enable dashboard
+minikube addons enable metallb
+
+kubectl apply -f srcs/metallb/metallb-system.yaml > /dev/null
+kubectl apply -f srcs/metallb/metallb.yaml > /dev/null
+
 #use docker daemon from minikube.
 eval $(minikube docker-env)
 
 #build docker images.
-echo "${GREEN}**Docker images build**${END}"
+echo "**********Docker images build**************"
 	
-docker build -t my_nginx srcs/nginx > /dev/null 2>&1
-docker build -t my_wordpress srcs/wordpress > /dev/null 2>&1
-docker build -t my_mysql srcs/mysql > /dev/null 2>&1
-docker build -t my_phpmyadmin srcs/phpmyadmin > /dev/null 2>&1
-docker build -t my_ftps srcs/ftps > /dev/null 2>&1
-docker build -t my_grafana srcs/grafana > /dev/null 2>&1
-docker build -t my_influxdb srcs/influxdb > /dev/null 2>&1
+docker build -t my_nginx srcs/nginx/ > /dev/null
+docker build -t my_mysql srcs/mysql/ > /dev/null
+docker build -t my_phpmyadmin srcs/phpmyadmin/ > /dev/null
+docker build -t my_wordpress srcs/wordpress/ > /dev/null
+docker build -t my_ftps srcs/ftps/ > /dev/null
+# docker build -t my_grafana srcs/grafana/
+# docker build -t my_influxdb srcs/influxdb/
 
-echo "${BLUE}**Docker build completed**${END}"
+echo "**********Docker build completed************"
+
+echo "**********Deploy init***********************"
+
+kubectl apply -f srcs/nginx/nginx.yaml
+kubectl apply -f srcs/mysql/mysql_service.yaml
+kubectl apply -f srcs/mysql/mysql_pvc.yaml
+kubectl apply -f srcs/mysql/mysql_deployment.yaml
+kubectl apply -f srcs/ftps/ftps.yaml
+kubectl apply -f srcs/phpmyadmin/phpmyadmin.yaml
+kubectl apply -f srcs/wordpress/wordpress.yaml
+# kubectl apply -f srcs/telegraf/telegraf_secret.yaml
+# kubectl apply -f srcs/telegraf/telegraf_config.yaml
+# kubectl apply -f srcs/telegraf/telegraf_deployment.yaml
+# kubectl apply -f srcs/grafana/grafana.yaml
+# kubectl apply -f srcs/influxdb/influxdb_secret.yaml
+# kubectl apply -f srcs/influxdb/influxdb.yaml
+
+echo "***********Deploy completed******************"
--- ft_services/srcs/mysql/srcs/start.sh
+++ ft_services/srcs/mysql/srcs/start.sh
@@ -1,11 +1,19 @@
+#!/bin/sh
+
+if [[ ! -d /run/mysqld ]]; then
+	mkdir -p /run/mysqld
+	chown -R mysql:mysql /run/mysqld
+fi
+
 # Install mysql database(mariadb-install-db is a symlink to mysql_install_db).
-mysql_install_db --user=ekim
+mysql_install_db --user=root --datadir=/var/lib/mysql/
 
 #create database for wordpress
-mysqld --user=ekim --execute="CREATE DATABASE wordpress;"
-mysqld --user=ekim --execute="CREATE USER 'ekim'@'%' IDENTIFIED BY 'password';"
-mysqld --user=ekim --execute="GRANT ALL PREVILEGES ON *.* to 'ekim'@'%' WITH GRANT OPTIONS;"
-mysqld --user=ekim --execute="FLUSH PREVILEGES;"
+mysqld --user=root --execute="FLUSH PREVILEGES; \
+CREATE DATABASE wordpress; \
+CREATE USER 'ekim'@'%' IDENTIFIED BY 'password'; \
+GRANT ALL PREVILEGES ON *.* to 'ekim'@'%' IDENTIFIED BY 'password' WITH GRANT OPTIONS; \
+FLUSH PREVILEGES;"
 
 #sever start
-mysqld --user=ekim
+mysqld --user=root
