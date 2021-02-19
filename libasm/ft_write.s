4d69c975114fc979dd28fc2cc1d726197cae9828
Eunju Kim <ekim@c4r4s6.42seoul.kr>
2021-02-19 10:03:12 +0900

    overload

--- libasm/ft_write.s
+++ libasm/ft_write.s
@@ -7,7 +7,7 @@ _ft_write:
 	mov		rax, 0x2000004
 	syscall
 	jc		error
-	jmp		exit
+	ret
 
 error:
 	push	rax			;backup error value
@@ -15,8 +15,4 @@ error:
 	pop		rdx			;save error value in rdx
 	mov		[rax], rdx	;give error() the error value.
 	mov		rax, -1		;return : -1
-	ret
-
-exit:
-	mov rax, rdx
 	ret
--- libasm/libasm.h
+++ libasm/libasm.h
@@ -1,12 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libasm.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ekim <ekim@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/23 02:15:53 by ekim              #+#    #+#             */
+/*   Updated: 2021/01/23 02:17:09 by ekim             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef LIBASM_H
 # define LIBASM_H
 
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <errno.h>
+# include <stdio.h>
+# include <string.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <errno.h>
 
 size_t		ft_strlen(const char *str);
 char		*ft_strcpy(char *dst, const char *src);
