52518d20aabb2cc5a17179895ea29a505a25e5f1
eunju-silver <yorkessw@gmail.com>
2021-05-03 18:49:20 +0900

    fixed conflicts in cpp02

--- moduleCpp/cpp02/ex01/Fixed.cpp
+++ moduleCpp/cpp02/ex01/Fixed.cpp
@@ -10,17 +10,29 @@ Fixed::~Fixed()
 	std::cout << "Destructor callled" << std::endl;
 }
 
-Fixed::Fixed(const Fixed& copy)
+Fixed::Fixed(const Fixed &copy)
 {
 	std::cout << "Copy constructor called" << std::endl;
 	*this = copy;
 }
 
-Fixed &Fixed::operator=(const Fixed &fixed)
+Fixed &Fixed::operator=(const Fixed &copy)
 {
-	_value = fixed.getRawBits();
+	_value = copy.getRawBits();
 	std::cout << "Assignation operator called" << std::endl;
 	return (*this);
+}
+
+Fixed::Fixed(int value)
+{
+	std::cout << "Int constructor called" << std::endl;
+	_value = value << _bits;
+}
+
+Fixed::Fixed(float value)
+{
+	std::cout << "Float constructor called" << std::endl;
+	_value = roundf(value * (1 << _bits));
 }
 
 void		Fixed::setRawBits(int const value)
@@ -30,6 +42,20 @@ void		Fixed::setRawBits(int const value)
 
 int			Fixed::getRawBits() const
 {
-	std::cout << "getRawBits member function called" << std::endl;
 	return (_value);
+}
+
+int			Fixed::toInt(void) const
+{
+	return (_value >> _bits);
+}
+
+float		Fixed::toFloat(void) const
+{
+	return ((float)_value / (1 << _bits));
+}
+
+std::ostream	&operator<<(std::ostream &os, const Fixed &fixed)
+{
+	return (os << fixed.toFloat());
 }
--- moduleCpp/cpp02/ex01/Fixed.hpp
+++ moduleCpp/cpp02/ex01/Fixed.hpp
@@ -3,23 +3,30 @@
 
 #include <iostream>
 #include <string>
+#include <cmath>
 
 class Fixed
 {
 
 private :
 	int					_value;
-	static const int	_fixed = 8;
+	static const int	_bits = 8;
 
 public :
 
 	Fixed();
+	~Fixed();
 	Fixed(const Fixed &copy);
-	Fixed&			operator=(const Fixed &fixed);
+	Fixed&			operator=(const Fixed &copy);
+	Fixed(int param);
+	Fixed(float param);
 	void			setRawBits(int const value);
 	int				getRawBits(void) const;
-	~Fixed();
+	int				toInt(void) const;
+	float			toFloat(void) const;
 
 };
+
+std::ostream	&operator<<(std::ostream &os, const Fixed &fixed);
 
 #endif
